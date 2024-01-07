d9db472e2e8f0db641c8fdd79af4406c615a82a1
moonjunyoung <kkunyeot0729@gmail.com>
2024-01-07 23:47:52 +0900

    test:접속 클라이언트 ip확인 코드를 추가한다

--- backend_main.py
+++ backend_main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
 from backend.mail.presentation import MailPresentation
@@ -20,6 +20,16 @@ app.add_middleware(
 @app.get("/", status_code=200)
 def haelth_check():
     return "haelth_check"
+
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    client_ip = request.client.host
+    request_path = request.url.path
+    response = await call_next(request)
+    # 로그에 클라이언트 IP와 요청 경로를 기록
+    print(f"Request from {client_ip} to {request_path}")
+    return response
 
 
 app.include_router(MailPresentation.router)
