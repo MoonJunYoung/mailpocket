48eda8e464db8e763ede8abb2644553d23c52cc4
moonjunyoung <kkunyeot0729@gmail.com>
2024-01-09 00:05:51 +0900

    test:슬랙앱연동 api 테스트 출력을 추가

--- backend/common/slack_api.py
+++ backend/common/slack_api.py
@@ -14,10 +14,11 @@ class SlackAPI:
     def install(self, code, user_id):
         url = "https://slack.com/api/oauth.v2.access"
         data = {"client_id": client_id, "client_secret": client_secret, "code": code}
-        response = requests.post(url, data=data).json()
-        access_token = response.get("access_token")
-        user_key = response.get("authed_user").get("id")
-        team_name = response.get("team").get("name")
+        response_json = requests.post(url, data=data)
+        print(response_json.text)
+        access_token = response_json.get("access_token")
+        user_key = response_json.get("authed_user").get("id")
+        team_name = response_json.get("team").get("name")
         channel = Channel(
             id=None,
             user_key=user_key,
