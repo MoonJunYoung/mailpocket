d579a761877a30f7050a6b464df824973b3a76d8
dnfbd <dnfbd@naver.com>
2024-01-30 22:21:48 +0900

    Amplitude 유저 데이터 전송 로직을 수정한다 test2.

--- frontend/src/components/Amplitude.tsx
+++ frontend/src/components/Amplitude.tsx
@@ -4,13 +4,13 @@ import { getUserData, Token } from '../api/api';
 
 const amplitudeApiKey = 'a64d3e4d62617b1eca2b9ab4864cb072';
 
-// const devAmplitudeApiKey = 'd6e91c2b0e6fdb035d5087cbd79c5cab';
+const devAmplitudeApiKey = 'd6e91c2b0e6fdb035d5087cbd79c5cab';
 
-// const ampKey = process.env.NODE_ENV === 'production' ? amplitudeApiKey : devAmplitudeApiKey;
+const ampKey = process.env.NODE_ENV === 'production' ? amplitudeApiKey : devAmplitudeApiKey;
 
 
 export const initializeAmplitude = async () => {
-  amplitude.getInstance().init(amplitudeApiKey);
+  amplitude.getInstance().init(ampKey);
 };
 
 export const AmplitudeSetUserId = async () => {
@@ -34,13 +34,12 @@ export const AmplitudeResetUserId = async () => {
 };
 
 
-
 export const sendEventToAmplitude = async (eventName: string, properties: any) => {
   try {
     console.log(`${eventName}: ${JSON.stringify(properties || {})}`)
-    // if (process.env.NODE_ENV === 'development') {
-    //   console.log(`${eventName}: ${JSON.stringify(properties || {})}`)
-    // }
+    if (process.env.NODE_ENV === 'development') {
+      console.log(`${eventName}: ${JSON.stringify(properties || {})}`)
+    }
     amplitude.getInstance().logEvent(eventName, properties);
   } catch (error) {
     console.error('Amplitude 초기화 중 오류 발생:', error);
