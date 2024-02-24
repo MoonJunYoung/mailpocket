a0bfdcd0fc43f7ea891246b60a9c54e713ede968
Bellsig2 <75826074+Bellsig2@users.noreply.github.com>
2024-02-24 16:35:23 +0900

    모바일 마이페이지 수정
    구독중인 뉴스레터가 없을 시 구독 페이지로 이동

--- frontend/src/mobile/MobileMyPage/index.tsx
+++ frontend/src/mobile/MobileMyPage/index.tsx
@@ -1,36 +1,39 @@
-import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { getMyPageNewsLetterDetail, getMyPageSubscribeData, Token } from '../../api/api'
-import { sendEventToAmplitude } from '../../components/Amplitude'
-import { MobileMyPageNav } from '../../components/mobileComponent/MobileNav'
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  getMyPageNewsLetterDetail,
+  getMyPageSubscribeData,
+  Token,
+} from "../../api/api";
+import { sendEventToAmplitude } from "../../components/Amplitude";
+import { MobileMyPageNav } from "../../components/mobileComponent/MobileNav";
 
-import MobileSummary from '../../components/mobileComponent/MobileSummary'
-import PageLoding from '../../components/PageLoding'
-import { NewsLetterDataType, SummaryItem } from '../../pages/SubscribePage'
-
+import MobileSummary from "../../components/mobileComponent/MobileSummary";
+import PageLoding from "../../components/PageLoding";
+import { NewsLetterDataType, SummaryItem } from "../../pages/SubscribePage";
 
 export interface NavNewsLetterDataType {
-  id: number,
-  s3_object_key: string,
-  subject: string,
-  read_link: string
-  summary_list: SummaryItem,
-  newsletter_id: number,
-  date: string,
-  from_name: string,
-  from_email: string,
-  html_body: string
+  id: number;
+  s3_object_key: string;
+  subject: string;
+  read_link: string;
+  summary_list: SummaryItem;
+  newsletter_id: number;
+  date: string;
+  from_name: string;
+  from_email: string;
+  html_body: string;
 }
 
-
 const MobileMyPage = () => {
-  const [mynewsletter, setMyNewsLetter] = useState<NewsLetterDataType[]>([])
-  const [mynewsletterdetail, setMyNewsLetterDetail] = useState<NavNewsLetterDataType[]>([])
+  const [mynewsletter, setMyNewsLetter] = useState<NewsLetterDataType[]>([]);
+  const [mynewsletterdetail, setMyNewsLetterDetail] = useState<
+    NavNewsLetterDataType[]
+  >([]);
   const [selectedItem, setSelectedItem] = useState(0);
 
   const navigate = useNavigate();
   const authToken = Token();
-
 
   useEffect(() => {
     if (!authToken) {
@@ -40,22 +43,29 @@ const MobileMyPage = () => {
     }
   }, [authToken, navigate]);
 
-
   const handlegetData = async () => {
     try {
-      const responseNewsLetterList = await getMyPageSubscribeData('testapi/newsletter?&subscribe_status=subscribed&sort_type=recent');
+      const responseNewsLetterList = await getMyPageSubscribeData(
+        "testapi/newsletter?&subscribe_status=subscribed&sort_type=recent"
+      );
       setMyNewsLetter(responseNewsLetterList.data);
       if (responseNewsLetterList.data.length > 0) {
-        const responseNewsLetterDetail = await getMyPageNewsLetterDetail(`testapi/newsletter/${selectedItem ? selectedItem : responseNewsLetterList.data[0].id}/last-mail`)
-        setMyNewsLetterDetail([responseNewsLetterDetail.data])
+        const responseNewsLetterDetail = await getMyPageNewsLetterDetail(
+          `testapi/newsletter/${
+            selectedItem ? selectedItem : responseNewsLetterList.data[0].id
+          }/last-mail`
+        );
+        setMyNewsLetterDetail([responseNewsLetterDetail.data]);
+      } else {
+        navigate("/mobile/subscribe");
       }
     } catch (error) {
       console.log("Api 데이터 불러오기 실패", error);
     }
-  }
+  };
 
   useEffect(() => {
-    handlegetData()
+    handlegetData();
   }, [selectedItem]);
 
   return (
@@ -69,16 +79,16 @@ const MobileMyPage = () => {
       <MobileSummary summaryNewsLetterData={mynewsletterdetail} />
       {mynewsletterdetail.map((data) => {
         return data.html_body !== null ? (
-          <div className='mt-10' dangerouslySetInnerHTML={{ __html: data.html_body }} />
+          <div
+            className="mt-10"
+            dangerouslySetInnerHTML={{ __html: data.html_body }}
+          />
         ) : (
           <PageLoding />
         );
       })}
     </div>
   );
+};
 
-}
-
-export default MobileMyPage
-
-
+export default MobileMyPage;
