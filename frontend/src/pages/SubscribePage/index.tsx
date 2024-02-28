e155fd659b4be46f5fa854183b9a15bc3f50a921
Bellsig2 <75826074+Bellsig2@users.noreply.github.com>
2024-02-28 23:28:29 +0900

    Update index.tsx

--- frontend/src/pages/SubscribePage/index.tsx
+++ frontend/src/pages/SubscribePage/index.tsx
@@ -254,10 +254,11 @@ const Subscribe = () => {
                     구독중인 뉴스레터
                   </h1>
                   <div
-                    className={`${newslettersubscribe.length > 4
-                      ? "flex"
-                      : "grid grid-cols-4"
-                      } overflow-x-auto  gap-4 custom-scrollbar pb-[15px] cursor-pointer`}
+                    className={`${
+                      newslettersubscribe.length > 4
+                        ? "flex"
+                        : "grid grid-cols-4"
+                    } overflow-x-auto  gap-4 custom-scrollbar pb-[15px] cursor-pointer`}
                   >
                     {newslettersubscribe.map((data) => (
                       <div
@@ -274,10 +275,11 @@ const Subscribe = () => {
                             </p>
                           </div>
                           <div
-                            className={`h-[250px] w-[285px] mb-7 ${seeMoreStates[data.id]
-                              ? "overflow-y-auto"
-                              : "overflow-hidden"
-                              } text-ellipsis custom-scrollbar px-5`}
+                            className={`h-[250px] w-[285px] mb-7 ${
+                              seeMoreStates[data.id]
+                                ? "overflow-y-auto"
+                                : "overflow-hidden"
+                            } text-ellipsis custom-scrollbar px-5`}
                           >
                             {data.mail && data.mail.summary_list ? (
                               Object.entries(data.mail.summary_list).map(
@@ -382,10 +384,11 @@ const Subscribe = () => {
                       </p>
                     </div>
                     <div
-                      className={`h-[250px] mb-7 ${seeMoreStates[data.id]
-                        ? "overflow-auto"
-                        : "overflow-hidden"
-                        } custom-scrollbar px-5`}
+                      className={`h-[250px] mb-7 ${
+                        seeMoreStates[data.id]
+                          ? "overflow-auto"
+                          : "overflow-hidden"
+                      } custom-scrollbar px-5`}
                     >
                       {data.mail && data.mail.summary_list ? (
                         Object.entries(data.mail.summary_list).map(
