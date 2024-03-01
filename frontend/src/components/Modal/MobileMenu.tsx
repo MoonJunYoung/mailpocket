bdb36e093f056d5fc044aa0ed3aafccdb05625f8
Bellsig2 <75826074+Bellsig2@users.noreply.github.com>
2024-03-01 20:52:33 +0900

    모바일 마이페이지 레이아웃 틀어짐 수정

--- frontend/src/components/Modal/MobileMenu.tsx
+++ frontend/src/components/Modal/MobileMenu.tsx
@@ -45,76 +45,84 @@ const MobileMenu = ({
   };
 
   return (
-    <div className="fixed inset-0 bg-stone-300 bg-opacity-50  ">
-      {/* 네비게이션 아이템 영역 */}
-      <div className="flex w-full bg-white relative h-screen">
-        <div className="flex flex-col overflow-auto ">
-          <div className="subscribe-scrollbar w-full flex flex-col items-center overflow-auto ">
-            {mynewsletter.map((data) => (
-              <div className="">
-                <div
-                  className={`text-center text-[13px] break-words break-keep border-b p-2 flex flex-col items-center justify-center`}
-                  key={data.id}
-                  onClick={() => {
-                    handleItemClick(data.id);
-                  }}
-                >
-                  <img
-                    className="w-[35px]"
-                    src={`/images/${data.id}.png`}
-                    alt={String(data.id)}
-                  />
-                  <p
-                    className={`font-semibold w-[65px]  ${
-                      selectedItem === data.id
-                        ? "border-customPurple border-solid border-b-4"
-                        : ""
-                    }`}
+    <div className="">
+      <div className="fixed inset-0 bg-stone-300 bg-opacity-50  z-10">
+        {/* 네비게이션 아이템 영역 */}
+        <div className="relative h-screen">
+          <div className="bg-white flex flex-col w-fit h-[100%]">
+            <div className="subscribe-scrollbar flex flex-col items-center overflow-y-auto  ">
+              {mynewsletter.map((data) => (
+                <div className="">
+                  <div
+                    className={`h-[120px] gap-3 text-center text-[13px] break-words break-keep border-b px-2 py-4 flex flex-col items-center justify-center`}
+                    key={data.id}
+                    onClick={() => {
+                      handleItemClick(data.id);
+                    }}
                   >
-                    {data.name}
-                  </p>
+                    <img
+                      className="w-[35px]"
+                      src={`/images/${data.id}.png`}
+                      alt={String(data.id)}
+                    />
+                    <span
+                      className={`font-semibold w-[65px] pb-2  ${
+                        selectedItem === data.id
+                          ? "border-customPurple border-solid border-b-4 "
+                          : ""
+                      }`}
+                    >
+                      {data.name}
+                    </span>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-          <div className="">
-            <div className="flex flex-col  ">
-              <div className="flex flex-col">
-                <ChangeButton></ChangeButton>
-                <div
-                  onClick={() => {
-                    setModalOpen(true);
-                  }}
-                  className="bg-[#EEEEEE]  size-[42px] mx-auto rounded-xl px-[10px] cursor-pointer"
-                >
-                  <img
-                    className="mx-auto w-[200px] h-full"
-                    src="images\setting.svg"
-                    alt=""
-                  />
-                </div>
-              </div>
-              <div className="p-3bg-gray-100 w-full text-center border-t py-3">
-                <span
-                  className="text-gray-500 font-semibold"
-                  onClick={handleLogOut}
-                >
-                  로그아웃
-                </span>
-              </div>
-              {ModalOpen && (
-                <SettingModal
-                  setOpenModal={setModalOpen}
-                  newsLetters={mynewsletter}
-                />
-              )}
+              ))}
             </div>
+            <div className="">
+              <div className="flex flex-col  ">
+                <div
+                  className="flex flex-col gap-5
+                "
+                >
+                  <ChangeButton></ChangeButton>
+
+                  <div
+                    onClick={() => {
+                      setModalOpen(true);
+                    }}
+                    className="bg-[#EEEEEE]  size-[42px] mx-auto rounded-xl px-[10px] cursor-pointer mb-5"
+                  >
+                    <img
+                      className="mx-auto w-[200px] h-full"
+                      src="images\setting.svg"
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <div className="p-3bg-gray-100 w-full text-center border-t py-3">
+                  <span
+                    className="text-gray-500 font-semibold"
+                    onClick={handleLogOut}
+                  >
+                    로그아웃
+                  </span>
+                </div>
+                {ModalOpen && (
+                  <SettingModal
+                    setOpenModal={setModalOpen}
+                    newsLetters={mynewsletter}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="absolute top-0 left-20">
+            <span className="cursor-pointer text-2xl" onClick={closeModal}>
+              <img src="images/close.svg" className="size-[30px]" alt="닫기" />
+            </span>
           </div>
         </div>
       </div>
-      {/* <span className="cursor-pointer pl-2 text-2xl" onClick={closeModal}>
-          <img src="images/close.svg" className="size-[30px]" alt="닫기" />
-        </span> */}
     </div>
   );
 };
