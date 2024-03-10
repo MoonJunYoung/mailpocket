329e2752e5740e8015768ff0fae06cd6919fe283
Bellsig2 <75826074+Bellsig2@users.noreply.github.com>
2024-03-10 20:29:07 +0900

    bottomSheet 컴포넌트

---
+++ frontend/src/components/BottomSheet/BottomSheet.tsx
@@ -0,0 +1,16 @@
+import { useState, useEffect, useRef } from "react";
+import { BottomSheet } from "react-spring-bottom-sheet";
+import "react-spring-bottom-sheet/dist/style.css";
+import { BottomSheetContent } from "./BottomSheetContent";
+
+export const Sheet = ({ open, setOpen }: any) => {
+  return (
+    <>
+      <BottomSheet header={false} open={open} scrollLocking={true}>
+        <div className="mt-4">
+          <BottomSheetContent setOpen={setOpen}></BottomSheetContent>
+        </div>
+      </BottomSheet>
+    </>
+  );
+};
