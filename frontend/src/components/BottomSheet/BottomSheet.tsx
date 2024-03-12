397c1038497792aa469cbab97bad9d36374d6cb8
Bellsig2 <75826074+Bellsig2@users.noreply.github.com>
2024-03-12 21:52:13 +0900

    바텀시트 컴포넌트 props 수정
    mailData 추가

--- frontend/src/components/BottomSheet/BottomSheet.tsx
+++ frontend/src/components/BottomSheet/BottomSheet.tsx
@@ -3,12 +3,15 @@ import { BottomSheet } from "react-spring-bottom-sheet";
 import "react-spring-bottom-sheet/dist/style.css";
 import { BottomSheetContent } from "./BottomSheetContent";
 
-export const Sheet = ({ open, setOpen }: any) => {
+export const Sheet = ({ open, setOpen, mailData }: any) => {
   return (
     <>
       <BottomSheet header={false} open={open} scrollLocking={true}>
         <div className="mt-4">
-          <BottomSheetContent setOpen={setOpen}></BottomSheetContent>
+          <BottomSheetContent
+            setOpen={setOpen}
+            mailData={mailData}
+          ></BottomSheetContent>
         </div>
       </BottomSheet>
     </>
