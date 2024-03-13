bf241d6bbdefdc25ca73b46fda0affddcac811b1
dnfbd <dnfbd@naver.com>
2024-03-14 01:30:26 +0900

    git actions 테스트를 진행한다.

--- frontend/src/mobile/MobileMyPage/index.tsx
+++ frontend/src/mobile/MobileMyPage/index.tsx
@@ -30,7 +30,7 @@ const MobileMyPage = () => {
   const [mynewsletterdetail, setMyNewsLetterDetail] = useState<
     NavNewsLetterDataType[]
   >([]);
-  console.log('dddssss')
+  console.log('dddss')
   const [selectedItem, setSelectedItem] = useState(0);
   const navigate = useNavigate();
   const authToken = Token();
