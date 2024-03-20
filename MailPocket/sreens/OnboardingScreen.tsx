bfd1c5dc5e3e1c7e8f1d2acce35422e6e4844931
dnfbd <dnfbd@naver.com>
2024-03-20 15:21:28 +0900

    리액트 네이티브 환경설정

---
+++ MailPocket/sreens/OnboardingScreen.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+const OnbodingScreen = () => {
+  return (
+    <View style={[styles.containerCenter, { gap: 12 }]}>
+      <Text style={{ marginTop: 60, fontSize: 20, fontWeight: 'bold', color: 'black' }}>어떤 뉴스레터를 좋아하세요?</Text>
+      <Text style={{ fontSize: 15, color: '#777777' }}>클릭해서 최근 요약 확인하고,쉽게 구독하기</Text>
+    </View>
+
+  )
+}
+
+const styles = StyleSheet.create({
+  containerCenter: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
+
+
+export default OnbodingScreen
